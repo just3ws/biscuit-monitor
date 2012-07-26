b4b93e816681d5bc8c275be1c870c61c090d3e2c
Michael D. Hall <mdh@just3ws.com>
2012-07-26 16:13:39 -0500

    added deps on geocoder, sqlite and activerecord

--- lib/biscuit-monitor.rb
+++ lib/biscuit-monitor.rb
@@ -1,16 +1,28 @@
 # encoding: utf-8
 
+require 'active_record'
 require 'biscuit-monitor/version'
 require 'colorize'
 require 'logger'
 require 'multi_json'
 require 'net/http'
+require 'sqlite3'
 require 'thor'
 require 'uri'
 
 module Biscuit
   module Monitor
     trap("SIGINT") { throw :ctrl_c }
+
+    class CLI < Thor
+      default_task :start
+
+      desc "start", "Start monitoring your biscuit."
+      method_option :device_ip, :default => "192.168.1.1", :aliases => "-d"
+      def start
+        Biscuit::Monitor::Monitor.new(options[:device_ip]).poll
+      end
+    end
 
     class Monitor
       def initialize(device_ip)
@@ -31,27 +43,19 @@ module Biscuit
 
       def cinr_foreground_color(cinr)
         case
-        when cinr > 24
-          :green
-        when (13..24).include?(cinr)
-          :light_green
-        when (8..12).include?(cinr)
-          :yellow
-        when (3..7).include?(cinr)
-          :light_red
-        when cinr < 3
-          :red
+        when cinr > 24 then :green
+        when (13..24).include?(cinr) then :light_green
+        when (8..12).include?(cinr) then :yellow
+        when (3..7).include?(cinr) then :light_red
+        when cinr < 3 then :red
         end
       end
 
       def rssi_foreground_color (rssi)
         case
-        when rssi > -50
-          :green
-        when rssi < -100
-          :red
-        else
-          :yellow
+        when rssi > -50 then :green
+        when rssi < -100 then :red
+        else :yellow
         end
       end
 
@@ -69,22 +73,15 @@ module Biscuit
 
             write message
             @logger.debug(response)
-
           rescue Errno::EHOSTUNREACH => err
-
             write "Cannot find the biscuit. Check your connection."
             @logger.error(err.inspect)
-
           rescue StandardError => err
-
             write "There was an error checking your biscuit. See the logfile for details."
             @logger.error(err.inspect)
-
           ensure
-
             sleep @seconds  # TODO when error increase length of time until next check to avoid spamming error log
           end until false
-
         end
       end
 
@@ -93,8 +90,6 @@ module Biscuit
         print message
         @last_message = message
       end
-
-
 
       def parse(document)
         data = {}
@@ -120,7 +115,6 @@ module Biscuit
         data
       end
 
-
       def scrub_response(document)
         document = document.split.join
         document.gsub!(/'/, '"')
@@ -130,17 +124,6 @@ module Biscuit
         document.gsub!(/(\w*):\[/, '"\1": [')
         document.gsub!(/":"/, '": "')
         document.gsub!(/,/, ', ')
-      end
-
-    end
-
-    class CLI < Thor
-      default_task :start
-
-      desc "start", "Start monitoring your biscuit."
-      method_option :device_ip, :default => "192.168.1.1", :aliases => "-d"
-      def start
-        Biscuit::Monitor::Monitor.new(options[:device_ip]).poll
       end
     end
   end
